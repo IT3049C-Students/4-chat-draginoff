ff8b9bdad129630f4602b86458b7c3498f5c7f85
draginoff <93340583+draginoff@users.noreply.github.com>
2022-06-05 15:03:58 -0400

    Completed UpdateMessages 2

---
+++ resources/js/index.js
@@ -0,0 +1,19 @@
+const nameInput = document.getElementById("my-name-input");
+const myMessage = document.getElementById("my-message");
+const sendButton = document.getElementById("send-button");
+const chatBox = document.getElementById("chat");
+
+
+async function updateMessages() {
+  // Fetch Messages
+  const messages = await fetchMessages();
+  // Loop over the messages. Inside the loop we will:
+      // get each message
+      // format it
+      // add it to the chatbox
+  let formattedMessages = "";
+  messages.forEach(message => {
+      formattedMessages += formatMessage(message, nameInput.value);
+  });
+  chatBox.innerHTML = formattedMessages;
+}
